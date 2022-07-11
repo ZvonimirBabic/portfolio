fed8d30119da7c46d211e2ca1dbd489531697ad7
BlackWW <zvobabic@hotmail.com>
2022-07-11 21:09:27 +0200

    Add: Global bindings

---
+++ lib/bindings/global_bindings.dart
@@ -0,0 +1,17 @@
+import 'package:get/get.dart';
+import 'package:portfolio/screens/home/home_controller.dart';
+
+import '../services/dio_service.dart';
+import '../services/logger_service.dart';
+import '../services/storage_service.dart';
+
+class GlobalBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get
+      ..put(LoggerService())
+      ..put(StorageService())
+      ..put(DioService())
+      ..put(HomeController());
+  }
+}
