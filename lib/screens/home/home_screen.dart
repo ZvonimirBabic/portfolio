7d4e4a3b0ea017205b03598fe9031b7a6caebc32
BlackWW <zvobabic@hotmail.com>
2022-07-12 20:52:47 +0200

    Fix: Linter

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -8,6 +8,8 @@ class HomeScreen extends StatelessWidget {
 
   final HomeController homeController = Get.find<HomeController>();
 
+  HomeScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) => Scaffold(
         body: Container(),
