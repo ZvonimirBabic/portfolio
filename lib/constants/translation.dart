e3ed47df7b1cbb4922b6ca7c4ecea131d8988107
BlackWW <zvobabic@hotmail.com>
2022-07-17 20:44:30 +0200

    Add: translations

--- lib/constants/translation.dart
+++ lib/constants/translation.dart
@@ -6,6 +6,22 @@ class Translation extends Translations {
         'en_US': <String, String>{
           'helloWorld': 'Hello world!',
           'appName': "Zvonimir Babić's portfolio app",
+          'darkTheme': 'Dark mode',
+          'lightTheme': 'Light mode',
+          'theme': 'Theme',
+          'fontSize': 'Font size',
+          'socials': 'Find me on:',
+          'education': 'My education:',
+          'experience': 'My experience:',
+          'interactive': 'Interactive examples:',
+          'about_me':
+              "Developer with a passion for mobile technologies. I enjoy doing what I know and learning what I don't. Looking forward to make ideas and design come to life in the palm of your hand.",
+          'about_me2': 'Based in Europe, Croatia.',
+          'quote1': 'We all make choices, but in the end, our choices make us',
+          'quote1Author': '- Andrew Ryan',
+          'quote2': 'Nec dei nec reges at homines soli',
+          'takeALookResume': 'Take a look at my ',
+          'ongoing': 'Ongoing',
         },
         'de_DE': <String, String>{
           'helloWorld': 'Hallo Welt!',
