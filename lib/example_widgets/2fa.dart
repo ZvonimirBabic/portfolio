53f008d42e99c6e62da3787d251e9be1770c5669
BlackWW <zvobabic@hotmail.com>
2022-07-17 21:00:43 +0200

    Add: 2FA widget WIP

--- lib/example_widgets/2fa.dart
+++ lib/example_widgets/2fa.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:intl_phone_field/intl_phone_field.dart';
+import 'package:portfolio/global_widgets/spacers.dart';
 
 class TwoFAWidget extends StatelessWidget {
   const TwoFAWidget({Key? key}) : super(key: key);
@@ -8,8 +10,23 @@ class TwoFAWidget extends StatelessWidget {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        const TextField(),
+        Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: IntlPhoneField(
+              decoration: const InputDecoration(
+                labelText: 'Phone Number',
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(),
+                ),
+              ),
+              initialCountryCode: 'IN',
+              onChanged: (phone) {
+                print(phone.completeNumber);
+              },
+            )),
+        const VerticalSpacer(),
         MaterialButton(
+          child: const Text('Log in'),
           onPressed: () {},
           color: Colors.blue,
         )
